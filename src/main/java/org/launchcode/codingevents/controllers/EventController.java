19ab93b495d1a59cf194a3781bf8267ca43964a2
Brad Rucker <bruck63@gmail.com>
2023-02-23 15:07:04 -0600

    Completed coding-events through Chapter 13 Bootstrap

--- src/main/java/org/launchcode/codingevents/controllers/EventController.java
+++ src/main/java/org/launchcode/codingevents/controllers/EventController.java
@@ -3,7 +3,9 @@ package org.launchcode.codingevents.controllers;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -11,21 +13,27 @@ import java.util.List;
 @Controller
 @RequestMapping("events")
 public class EventController {
+
+    private static List<String> events = new ArrayList<>();
+
     @GetMapping
     public String displayAllEvents(Model model) {
-        List<String> events = new ArrayList<>();
-        events.add("Code With Pride");
-        events.add("Strange Loop");
-        events.add("Apple WWDC");
-        events.add("SpringOne Platform");
         model.addAttribute("events", events);
         return "events/index";
     }
 
-    //lives at /events/create
+    //lives at /events/create - Creates Form
     @GetMapping("create")
     public String renderCreateEventForm() {
         return "events/create";
+    }
+
+    //Handles Form
+    //Lives at /events/create - Handles different type of request as (renderCreateEventForm) so it is ok to be at same location
+    @PostMapping("create")
+    public String createEvent(@RequestParam String eventName) {
+        events.add(eventName);
+        return "redirect:";
     }
 
 }
