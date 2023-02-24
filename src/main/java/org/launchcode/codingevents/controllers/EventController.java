82349575d2e074dd7d0190222d7e1f2bd8bfdcf5
Brad Rucker <bruck63@gmail.com>
2023-02-23 20:03:32 -0600

    Completed through 15.3 and displayed genereic erorr message on create form when errors are thrown

--- src/main/java/org/launchcode/codingevents/controllers/EventController.java
+++ src/main/java/org/launchcode/codingevents/controllers/EventController.java
@@ -1,12 +1,14 @@
 package org.launchcode.codingevents.controllers;
 
+
+import org.launchcode.codingevents.data.EventData;
+import org.launchcode.codingevents.models.Event;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.validation.Errors;
+import org.springframework.web.bind.annotation.*;
 
+import javax.validation.Valid;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -14,25 +16,51 @@ import java.util.List;
 @RequestMapping("events")
 public class EventController {
 
-    private static List<String> events = new ArrayList<>();
-
     @GetMapping
     public String displayAllEvents(Model model) {
-        model.addAttribute("events", events);
+        model.addAttribute("events", EventData.getAll());
+        model.addAttribute("title", "All Events");
         return "events/index";
     }
 
     //lives at /events/create - Creates Form
     @GetMapping("create")
-    public String renderCreateEventForm() {
+    public String renderCreateEventForm(Model model) {
+        model.addAttribute("title", "Create Event");
         return "events/create";
     }
 
-    //Handles Form
+    //Handles add Form
     //Lives at /events/create - Handles different type of request as (renderCreateEventForm) so it is ok to be at same location
     @PostMapping("create")
-    public String createEvent(@RequestParam String eventName) {
-        events.add(eventName);
+    public String createEvent(@ModelAttribute @Valid Event newEvent, Errors errors, Model model) {
+
+        if(errors.hasErrors()) {
+            model.addAttribute("title", "Create Event");
+            model.addAttribute("errorMsg", "Bad data!");
+            return "events/create";
+        }
+
+        EventData.add(newEvent);
+        return "redirect:";
+    }
+
+    //Handles Delete Form
+    @GetMapping("delete")
+    public String displayDeleteEventForm(Model model) {
+        model.addAttribute("title", "Delete Events");
+        model.addAttribute("events", EventData.getAll());
+        return "events/delete";
+    }
+
+    // Controls the delete action from the delete template
+    @PostMapping("delete")
+    public String processDeleteEventForm(@RequestParam(required = false) int[] eventIds) { //eventIds matches the name in the delete form
+        if(eventIds != null) {
+            for (int id : eventIds) {
+                EventData.remove(id);
+            }
+        }
         return "redirect:";
     }
 
