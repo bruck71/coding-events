533deb36514832b188325b6976102dc48c401750
Brad Rucker <bruck63@gmail.com>
2023-03-01 16:32:29 -0600

    Added an enum class for EventTypes.  The ID incrementation by 2 is not an issue since all that is required is a unique ID number per Object.

--- src/main/java/org/launchcode/codingevents/models/Event.java
+++ src/main/java/org/launchcode/codingevents/models/Event.java
@@ -19,17 +19,19 @@ public class Event {
     @Email(message = "Invalid email. Try again.")
     private String contactEmail;
 
-    public Event() {
-        this.id = nextId;
-        nextId ++;
-    }
+    private EventType type;
 
-    public Event(String name, String description, String contactEmail) {
+    public Event(String name, String description, String contactEmail, EventType type) {
         this();
         this.name = name;
         this.description = description;
         this.contactEmail = contactEmail;
+        this.type = type;
+    }
 
+    public Event() {
+        this.id = nextId;
+        nextId ++;
     }
 
     public String getName() {
@@ -58,6 +60,14 @@ public class Event {
 
     public void setContactEmail(String contactEmail) {
         this.contactEmail = contactEmail;
+    }
+
+    public EventType getType() {
+        return type;
+    }
+
+    public void setType(EventType type) {
+        this.type = type;
     }
 
     @Override
